2648c53ca8219d117a6c16925542e0974aef0bda
xhad <chad@loreum.org>
2025-02-15 21:23:51 +0800

    chore: refactor folder, add readme, dag simulation

--- network/node.go
+++ network/node.go
@@ -1,8 +1,7 @@
-package main
+package network
 
 import (
 	"crypto/ed25519"
-	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
@@ -37,13 +36,13 @@ type Node struct {
 	Peers        []peer.ID
 }
 
-// GenerateKeys creates a new key pair for signing transactions.
-func GenerateKeys() (ed25519.PublicKey, ed25519.PrivateKey) {
-	publicKey, privateKey, _ := ed25519.GenerateKey(rand.Reader)
-	return publicKey, privateKey
+// Multi-Hop Routing: Propagates transactions to peers in libp2p network.
+func (n *Node) MultiHopGossip(tx Transaction) {
+	for _, peerID := range n.Peers {
+		fmt.Printf("Node %s sending transaction %s to peer %s\n", n.ID, tx.ID, peerID.String())
+	}
 }
 
-// CreateTransaction generates a signed transaction.
 func (n *Node) CreateTransaction(data string, parents []string) Transaction {
 	hash := sha256.Sum256([]byte(fmt.Sprintf("%s:%v", data, time.Now().UnixNano())))
 	txID := hex.EncodeToString(hash[:])
@@ -100,13 +99,6 @@ func (n *Node) GossipTransaction(tx Transaction, nodes []*Node) {
 	}
 }
 
-// Multi-Hop Routing: Propagates transactions to peers in libp2p network.
-func (n *Node) MultiHopGossip(tx Transaction) {
-	for _, peerID := range n.Peers {
-		fmt.Printf("Node %s sending transaction %s to peer %s\n", n.ID, tx.ID, peerID.String())
-	}
-}
-
 // FinalizeTransaction: Ensures consensus by checkpointing transactions.
 func (n *Node) FinalizeTransaction(txID string) {
 	n.mu.Lock()
@@ -126,59 +118,4 @@ func SetupLibp2pNode() host.Host {
 		panic(err)
 	}
 	return h
-}
-
-// DAG-based aBFT Simulation with Multi-Hop Gossiping and Finality
-func main() {
-	// Generate keys for each node
-	pubA, privA := GenerateKeys()
-	pubB, privB := GenerateKeys()
-	pubC, privC := GenerateKeys()
-
-	// Create libp2p nodes
-	hostA := SetupLibp2pNode()
-	hostB := SetupLibp2pNode()
-	hostC := SetupLibp2pNode()
-
-	// Create DAG nodes
-	nodeA := &Node{ID: "A", Transactions: make(map[string]Transaction), PrivateKey: privA, PublicKey: pubA, Reputation: 100, Host: hostA}
-	nodeB := &Node{ID: "B", Transactions: make(map[string]Transaction), PrivateKey: privB, PublicKey: pubB, Reputation: 100, Host: hostB}
-	nodeC := &Node{ID: "C", Transactions: make(map[string]Transaction), PrivateKey: privC, PublicKey: pubC, Reputation: 100, Host: hostC}
-	nodes := []*Node{nodeA, nodeB, nodeC}
-
-	// Connect nodes for libp2p multi-hop communication
-	nodeA.Peers = []peer.ID{hostB.ID(), hostC.ID()}
-	nodeB.Peers = []peer.ID{hostA.ID(), hostC.ID()}
-	nodeC.Peers = []peer.ID{hostA.ID(), hostB.ID()}
-
-	// Genesis transaction
-	genesisTx := nodeA.CreateTransaction("Genesis", []string{})
-	nodeA.GossipTransaction(genesisTx, nodes)
-
-	// New transaction with Genesis as parent
-	tx1 := nodeB.CreateTransaction("Tx1", []string{genesisTx.ID})
-	if nodeB.ValidateTransaction(tx1) {
-		nodeB.GossipTransaction(tx1, nodes)
-		nodeB.MultiHopGossip(tx1)
-		fmt.Println("Tx1 added and propagated.")
-	}
-
-	// Another transaction with Tx1 as parent
-	tx2 := nodeC.CreateTransaction("Tx2", []string{tx1.ID})
-	if nodeC.ValidateTransaction(tx2) {
-		nodeC.GossipTransaction(tx2, nodes)
-		nodeC.MultiHopGossip(tx2)
-		fmt.Println("Tx2 added and propagated.")
-	}
-
-	// Finalizing transactions after propagation
-	nodeA.FinalizeTransaction(genesisTx.ID)
-	nodeB.FinalizeTransaction(tx1.ID)
-	nodeC.FinalizeTransaction(tx2.ID)
-
-	// Print finalized transactions in Node A
-	fmt.Println("Node A Transactions (Finalized Status):")
-	for _, tx := range nodeA.Transactions {
-		fmt.Printf("ID: %s, Data: %s, Parents: %v, Finalized: %v\n", tx.ID, tx.Data, tx.Parents, tx.Finalized)
-	}
 }
